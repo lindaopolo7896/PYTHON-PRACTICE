74412cc294c469317b76b4e64a845b4cf2ed7917
lindaopolo7896 <eleanor.opolo@strathmore.edu>
2025-04-26 14:13:53 +0300

    Fifth commit

---
+++ recursive.py
@@ -0,0 +1,10 @@
+def recursive_function(x):
+  #base case
+  if x == 0 or x == 1:
+    return 1
+  #recursive case
+  else:
+    return x * recursive_function(x - 1)
+  
+# Test the recursive function
+print(recursive_function(5))
