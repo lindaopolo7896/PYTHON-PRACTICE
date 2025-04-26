52a3b1cc457ae087a68095fd16d250c38b91b21d
lindaopolo7896 <eleanor.opolo@strathmore.edu>
2025-04-26 15:23:19 +0300

    Sixth Commit

---
+++ digits.py
@@ -0,0 +1,9 @@
+def sum_digits(n):
+  total = 0
+  while n > 0:
+    total += n % 10 # Add the last digit to total
+    n //= 10 # Remove the last digit from n
+  return total
+
+# Test the function
+print(sum_digits(12345)) 
